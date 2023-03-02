c71623d45365cd60972175afc66b9d4168e88119
B1gM8c <89020353+B1gM8c@users.noreply.github.com>
2023-03-02 17:56:51 +0800

    更新抖音解析接口，支持X-Bogus校验
    由于本人不是PHP专业的，代码可能比较鸡肋，能用就行了:)如果你想部署自己的本地X-Bogus生成接口，也可以参考相关的开源项目
    https://github.com/B1gM8c/X-Bogus

--- src/video_spider.php
+++ src/video_spider.php
@@ -34,14 +34,60 @@ class Video {
     public function douyin($url) {
         $loc = get_headers($url, true) ['Location'];
         preg_match('/video\/(.*)\?/', $loc, $id);
-        // 接口已于失效
-        // $arr = json_decode($this->curl('https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=' . $id[1]), true);
         $num = preg_replace('/[^0-9]/', '', $id[1]);
-        $arr = json_decode($this->curl('https://www.iesdouyin.com/aweme/v1/web/aweme/detail/?aweme_id=' .$num), true);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        //你也可以选择部署本地接口，生成X-Bogus，开源地址 https://github.com/B1gM8c/X-Bogus
+           CURLOPT_URL => 'https://tiktok.iculture.cc/X-Bogus',
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_ENCODING => '',
+           CURLOPT_MAXREDIRS => 10,
+           CURLOPT_TIMEOUT => 0,
+           CURLOPT_FOLLOWLOCATION => true,
+           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+           CURLOPT_CUSTOMREQUEST => 'POST',
+           CURLOPT_POSTFIELDS =>'{
+            "url":"https://www.douyin.com/aweme/v1/web/aweme/detail/?aweme_id='.$num.'&aid=1128&version_name=23.5.0&device_platform=android&os_version=2333",
+            "user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+        }',
+           CURLOPT_HTTPHEADER => array(
+              'User-Agent: FancyPig',
+              'Content-Type: application/json',
+              'Accept: */*',
+              'Host: tiktok.iculture.cc',
+              'Connection: keep-alive'
+           ),
+        ));
+        
+        $json_array= json_decode(curl_exec($curl));
+        curl_close($curl);
+        $new_url = $json_array->param;
+        $msToken = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 107);
+        
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+           CURLOPT_URL => $new_url,
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_ENCODING => '',
+           CURLOPT_MAXREDIRS => 10,
+           CURLOPT_TIMEOUT => 0,
+           CURLOPT_FOLLOWLOCATION => true,
+           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+           CURLOPT_CUSTOMREQUEST => 'GET',
+           CURLOPT_HTTPHEADER => array(
+              'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+              'Referer: https://www.douyin.com/',
+              'Cookie: msToken='.$msToken.';odin_tt=324fb4ea4a89c0c05827e18a1ed9cf9bf8a17f7705fcc793fec935b637867e2a5a9b8168c885554d029919117a18ba69; ttwid=1%7CWBuxH_bhbuTENNtACXoesI5QHV2Dt9-vkMGVHSRRbgY%7C1677118712%7C1d87ba1ea2cdf05d80204aea2e1036451dae638e7765b8a4d59d87fa05dd39ff; bd_ticket_guard_client_data=eyJiZC10aWNrZXQtZ3VhcmQtdmVyc2lvbiI6MiwiYmQtdGlja2V0LWd1YXJkLWNsaWVudC1jc3IiOiItLS0tLUJFR0lOIENFUlRJRklDQVRFIFJFUVVFU1QtLS0tLVxyXG5NSUlCRFRDQnRRSUJBREFuTVFzd0NRWURWUVFHRXdKRFRqRVlNQllHQTFVRUF3d1BZbVJmZEdsamEyVjBYMmQxXHJcbllYSmtNRmt3RXdZSEtvWkl6ajBDQVFZSUtvWkl6ajBEQVFjRFFnQUVKUDZzbjNLRlFBNUROSEcyK2F4bXAwNG5cclxud1hBSTZDU1IyZW1sVUE5QTZ4aGQzbVlPUlI4NVRLZ2tXd1FJSmp3Nyszdnc0Z2NNRG5iOTRoS3MvSjFJc3FBc1xyXG5NQ29HQ1NxR1NJYjNEUUVKRGpFZE1Cc3dHUVlEVlIwUkJCSXdFSUlPZDNkM0xtUnZkWGxwYmk1amIyMHdDZ1lJXHJcbktvWkl6ajBFQXdJRFJ3QXdSQUlnVmJkWTI0c0RYS0c0S2h3WlBmOHpxVDRBU0ROamNUb2FFRi9MQnd2QS8xSUNcclxuSURiVmZCUk1PQVB5cWJkcytld1QwSDZqdDg1czZZTVNVZEo5Z2dmOWlmeTBcclxuLS0tLS1FTkQgQ0VSVElGSUNBVEUgUkVRVUVTVC0tLS0tXHJcbiJ9',
+              'Accept: */*',
+              'Host: www.douyin.com',
+              'Connection: keep-alive'
+           ),
+        ));
+        $arr = json_decode(curl_exec($curl), true);
+        curl_close($curl);
         if ($arr['status_code']==0) {
             $arr = ['code' => 200, 
             'msg' => '解析成功', 
-            'data' => [
                 'author' => $arr['aweme_detail']['author']['nickname'], 
                 'uid' => $arr['aweme_detail']['author']['unique_id'], 
                 'avatar' => $arr['aweme_detail']['music']['avatar_large']['url_list'][0],
@@ -55,7 +101,6 @@ class Video {
                     'author' => $arr['aweme_detail']['music']['author'], 
                     'avatar' => $arr['aweme_detail']['music']['cover_large']['url_list'][0], 
                     'url' => $arr['aweme_detail']['music']['play_url']['url_list'][0], 
-                ]
                 ]
             ];
             return $arr;
